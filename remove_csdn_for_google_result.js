45f92d3c53dade8109672a22695351fed6577c98
Atever Yuan <ateveryuan@gmail.com>
2019-10-08 21:01:38 +0800

    correct namespace

--- remove_csdn_for_google_result.js
+++ remove_csdn_for_google_result.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google 搜索结果去除 CSDN
-// @namespace    https://github.com/atever/browserfork/edit/master/ituring.js
+// @namespace    https://raw.githubusercontent.com/atever/browserfork/master/remove_csdn_for_google_result.js
 // @version      0.1.1
 // @description  Google 搜索结果去除 CSDN
 // @license      MIT License
