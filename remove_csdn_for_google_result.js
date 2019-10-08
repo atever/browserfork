126dc9966d5dedc81fbe999590ca5997196576ce
Atever Yuan <ateveryuan@gmail.com>
2019-10-08 21:12:42 +0800

    Create remove_csdn_for_google_result.js

---
+++ remove_csdn_for_google_result.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Google 搜索结果去除 CSDN
+// @namespace    https://raw.githubusercontent.com/atever/browserfork/master/remove_csdn_for_google_result.js
+// @version      0.1.1
+// @description  remove_csdn_for_google_result.js
+// @license      MIT License
+// @author       ateveryuan@gmail.com
+// @match        https://*.google.com/*
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    var searchQuery = window.location.search;
+    var reg = /(&q=|\?q=).*&oq=/;
+    if(searchQuery.indexOf("csdn") === -1 && reg.test(searchQuery)) {
+        window.location.href = addFWORD(searchQuery);
+    }
+    function addFWORD(searchQuery) {
+        var str = searchQuery.split('&oq=');
+        return str[0].concat('+-csdn&oq=').concat(str[1]);
+    }
+})();
